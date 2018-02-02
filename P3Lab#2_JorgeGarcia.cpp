a60397dd7243a7bd9d69be673292e3f425d959b0
JorgeGarcia <Jorgegar896@gmail.com>
2018-02-02 14:12:29 -0600

    Menus y comprobacion de triangular listos

--- P3Lab#2_JorgeGarcia.cpp
+++ P3Lab#2_JorgeGarcia.cpp
@@ -8,6 +8,7 @@ int menuPrincipal(){
 	    <<"1) Ejercicio 1"   <<endl
 	    <<"2) Ejercicio 2"   <<endl
 	    <<"3) Ejercicio 3"   <<endl
+	    <<"4) Salir"	 <<endl
 	    <<"--------------- " <<endl
 	;
 	cin >> opcion;
@@ -19,6 +20,7 @@ int menuEjercicio1(){
 	    <<"1)Compromar numero ingresado"	   <<endl
 	    <<"2)Comprobar numero al azar"	   <<endl
 	    <<"3)Comprobar triangular mas cercano" <<endl
+	    <<"4)Salir"				   <<endl
 	    <<"----------------------------------" <<endl
 	;
 	cin >> opcion;
@@ -28,12 +30,12 @@ int menuEjercicio2();
 
 int menuEjercicio3();
 
-//Ejercicio 1
-ejercicio1()
+//Ejercicio de los numeros triangulares y sus prototipos de funciones
+void ejercicio1();
 
-bool comprobarTriangular(int);
+void comprobarTriangular(int);
 
-bool triangularMasCercano(int);
+void triangularMasCercano(int);
 
 int main(){
 	bool salir = false;
@@ -54,4 +56,39 @@ int main(){
 		}
 	}
 	return 0;	
+}
+
+void Ejercicio1(){
+	bool salir = false;
+	int numero = 0;
+	while(!salir){
+		switch (menuEjercicio1){
+			case 1:
+				cout<<"Ingrese un numero";
+				cin>>numero;
+				comprobarTriangular(numero);		
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				salir = true;
+				break;
+			default:
+				cout<<"Opcion no valida, porfavor ingrese otro numero"<<endl;
+		}
+	}	
+}
+
+void comprobarTrinagular(int numero)
+	int suma_triangular = 0;
+	for (int i=1; i <= numero; i++){
+		suma_triangular =+ i;
+		if (suma_triangular == numero){
+			cout<<"El numero es triangular"<<endl;
+		}else if(i == numero){
+			cout<<"El numero no es triangular"<<endl;
+		}
+	}
 }
